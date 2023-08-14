88023224b737ee8216f5263b071f9905887e3aa2
YE-JIMIN <jm567890@naver.com>
2023-08-15 03:02:24 +0900

    Implement post api

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,8 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +12,12 @@ public class CalculatorController {
 
     @GetMapping("/add") //get /add
     public int addToNumbers(CalculatorAddRequest request) {
-        return  request.getNumber1() + request.getNumber2();
+        return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")   //post /multiply
+    public int multiplyToNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
     }
 
 }
