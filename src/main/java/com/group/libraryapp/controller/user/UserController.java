7b9eb55e1c2af8598d6ce1f5ababb65e2d9ad5fa
LAPTOP-132H6FU8\YJM <jm567890@gsitm.com>
2023-09-05 11:11:33 +0900

    git ignore add

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,30 +3,49 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    //    private final List<User> users = new ArrayList<>();//메모리 저장 삭제
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) { //생성자 자동주입
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user") //post /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+//        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO USER (NAME, AGE) VALUES (?,?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for(int i = 0 ; i  < users.size(); i++){
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-        return responses;
+        String sql = "SELECT * FROM USER";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> { //USER 정보를 UserResponse로 바꿈
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            Integer age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+//        List<UserResponse> responses = new ArrayList<>();
+//
+//        for (int i = 0; i < users.size(); i++) {
+//            responses.add(new UserResponse(i + 1, users.get(i)));
+//        }
+//        return responses;
     }
 }
